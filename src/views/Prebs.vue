afb4f1d8af7d08da6026b6d068b73f1a56847d0b
flydedog <l1141041@163.com>
2021-06-26 14:17:47 +0800

    rewrite Prebs

--- src/views/Prebs.vue
+++ src/views/Prebs.vue
@@ -8,7 +8,20 @@
                 <el-tree :data="aside_future_data" :props="$store.state.defaultProps" @node-click="aside_data_click"></el-tree>
             </el-aside>
             <el-main>
-                <div class="title_level_two">震荡----不做, 趋势策略死于震荡, 震荡策略死于趋势, 混着做, 取死之道</div>
+                <ol v-for="(item, id) in note_data" :key="id">
+                    <li>
+                        <div class="title_level_two">{{item.conception}}</div>
+                        <el-divider></el-divider>
+                        <el-table ref="multipleTable" :data="item.condition" tooltip-effect="dark" style="width: 100%" @selection-change="handleSelectionChange">
+                            <el-table-column type="selection" width="55"></el-table-column>
+                            <el-table-column prop="" label="" width="70"></el-table-column>
+                            <el-table-column prop="condition" label="条件" width="1200"></el-table-column>
+                        </el-table>
+                        <el-divider></el-divider>
+                    </li>
+                </ol>
+
+                <!-- <div class="title_level_two">震荡----不做, 趋势策略死于震荡, 震荡策略死于趋势, 混着做, 取死之道</div>
                 <el-divider></el-divider>
                 <div class="title_level_two">趋势----开仓</div>
                 <el-divider></el-divider>
@@ -40,7 +53,7 @@
                     <el-table-column type="selection" width="55"></el-table-column>
                     <el-table-column prop="" label="" width="70"></el-table-column>
                     <el-table-column prop="condition" label="条件" width="1200"></el-table-column>
-                </el-table>
+                </el-table> -->
 
             </el-main>
         </el-container>
@@ -49,47 +62,75 @@
 </template>
 
 <script>
-import state_tree from '../components/print/state_tree.vue'
-import { trend_common_data, trend_open_data, trend_add_data, trend_reduce_data, trend_close_data } from '../../config.host.js'
-
+import ajax from '../assets/ajax.js'
 export default {
     data() {
         return {
-            trend_common_data: trend_common_data,
-            trend_open_data: trend_open_data,
-            trend_add_data: trend_add_data,
-            trend_reduce_data: trend_reduce_data,
-            trend_close_data: trend_close_data,
-            multipleSelection: []
+            // trend_open_data: trend_open_data,
+            // trend_add_data: trend_add_data,
+            // trend_reduce_data: trend_reduce_data,
+            // trend_close_data: trend_close_data,
+            multipleSelection: [],
+            note_data: []
         }
     },
     computed: {
         aside_future_data() {
             return this.$store.getters['future_info/aside_future_data']
-        },
-        trend_open() {
-            return [...this.trend_common_data, ...this.trend_open_data]
-        },
-        trend_add() {
-            return [...this.trend_common_data, ...this.trend_add_data]
-        },
-        trend_reduce() {
-            return this.trend_reduce_data
-        },
-        trend_close() {
-            return this.trend_close_data
         }
+        // trend_open() {
+        //     return this.trend_open_data
+        // },
+        // trend_add() {
+        //     return this.trend_add_data
+        // },
+        // trend_reduce() {
+        //     return this.trend_reduce_data
+        // },
+        // trend_close() {
+        //     return this.trend_close_data
+        // }
+    },
+    mounted() {
+        ajax.post({
+            url: 'note/get_json_data',
+            data: {
+                key_name: 'prebs'
+            }
+        })
+            .then((response) => {
+                if (response.status === 200) {
+                    let data_format = []
+                    for (let index in response.data) {
+                        let data_one = response.data[index]
+                        let condition_data_one = []
+                        for (let index_2 in data_one.condition) {
+                            condition_data_one.push({
+                                condition: data_one.condition[index_2]
+                            })
+                        }
+                        data_format.push({
+                            conception: data_one.conception,
+                            condition: condition_data_one
+                        })
+                    }
+
+                    this.note_data = data_format
+                }
+            })
+            .catch((err) => {
+                console.log(err)
+            })
     },
     methods: {
         aside_data_click(data) {},
+
         handleSelectionChange(val) {
             console.log(val)
             // this.multipleSelection = val;
         }
     },
-    components: {
-        state_tree
-    }
+    components: {}
 }
 </script>
 
